e67ee67c76fddaf7fc3e90db09d6cbed8c2c39b9
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-02-17 22:37:22 +0100

    summary data display

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
     <ul>
-      <li v-for="(value, key) in summary" :key="value.identifier">
-        {{ key }} : {{ value }}
-      </li>
+      <table>
+        <thead>
+          <tr>
+            <th>Phone number</th>
+            <th>Number of calls</th>
+            <th>Last call details</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(value, key) in summary" :key="value.identifier">
+            <td>{{ key }}</td>
+            <td>{{ value.callCount }} {{ value.callCount > 1 ? 'calls' : 'call' }} </td>
+            <td>{{ value.last.agentName }} / {{formatDate(value.last.dateTime)}} </td>
+          </tr>
+        </tbody>
+      </table>
     </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import dayjs from 'dayjs';
 import fetchData from '../api';
 
 export default {
@@ -25,6 +39,18 @@ export default {
       const result = await fetchData('summary');
       this.summary = result;
     },
+    formatDate(date) {
+      return dayjs(date).format('HH:mm');
+    },
   },
 };
 </script>
+
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+  padding: .5rem;
+}
+
+</style>
